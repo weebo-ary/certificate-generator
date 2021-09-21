911b30bf1493714020b08ecb2555b9c43009adef
Aryan Bhardwaj <74592979+weebo-ary@users.noreply.github.com>
2021-09-21 23:45:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+const userName = document.getElementById("name");
+const submitBtn = document.getElementById("submitBtn");
+
+const { PDFDocument, rgb, degrees } = PDFLib;
+
+
+const capitalize = (str, lower = false) =>
+  (lower ? str.toLowerCase() : str).replace(/(?:^|\s|["'([{])+\S/g, (match) =>
+    match.toUpperCase()
+  );
+
+submitBtn.addEventListener("click", () => {
+  const val = capitalize(userName.value);
+  if (val.trim() !== "" && userName.checkValidity()) {
+    generatePDF(val);
+  } else {
+    userName.reportValidity();
+  }
+});
+
+const generatePDF = async (name) => {
+  const existingPdfBytes = await fetch("./certificate.pdf").then((res) =>
+    res.arrayBuffer()
+  );
+  const pdfDoc = await PDFDocument.load(existingPdfBytes);
+  pdfDoc.registerFontkit(fontkit);
+  const fontBytes = await fetch("./Sanchez-Regular.ttf").then((res) =>
+    res.arrayBuffer()
+  );
+  const SanChezFont = await pdfDoc.embedFont(fontBytes);
+  const pages = pdfDoc.getPages();
+  const firstPage = pages[0];
+  firstPage.drawText(name, {
+    x: 420,
+    y: 300,
+    size: 28,
+    font: SanChezFont,
+    color: rgb(0, 0, 0  ),
+  });
+  const pdfBytes = await pdfDoc.save();
+  console.log("Done creating");
+  var file = new File(
+    [pdfBytes],
+    "Padhega India Subscription Certificate.pdf",
+    {
+      type: "application/pdf;charset=utf-8",
+    }
+  );
+ saveAs(file);
+};
